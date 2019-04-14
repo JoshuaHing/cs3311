a370d826458fd1165d52e8cdea4d9a6c312d71a2
JoshuaHing <z5117173@ad.unsw.edu.au>
2019-04-15 05:45:06 +1000

    (almost) finished with F

--- ass2/test2.sql
+++ ass2/test2.sql
@@ -8,16 +8,13 @@ WITH RECURSIVE RecursiveCTE
          SELECT
                 prev.end_actor,
                 prev.Level + 1
-         FROM   (RecursiveCTE R
-                join e_movie_actor E
-                on R.end = E.start_actor and R.Level < 2) as prev)
-                /*
+         FROM   RecursiveCTE R
                 JOIN ACTING c1
                   ON c1.movie_id = R.movie_id
                      AND R.Level < 2
                 JOIN ACTING c2
                   ON c1.actor_id = c2.actor_id)
-                  */
+
 Select R.end, R.level
 FROM   RecursiveCTE R
 group by R.end, R.level;
@@ -38,12 +35,12 @@ WITH RECURSIVE RecursiveCTE
          FROM   RecursiveCTE R
                 JOIN acting c1
                   ON c1.movie_id = R.movie_id
-                     AND R.Level < 2
+                     AND R.Level < 6
                 JOIN acting c2
                   ON c1.actor_id = c2.actor_id)
-SELECT actor_id
+SELECT actor_id, level
 FROM   RecursiveCTE
-GROUP  BY actor_id
+GROUP  BY actor_id, level
 
 with recursive F_degrees
      as (select
@@ -66,6 +63,8 @@ where R.end != 1598 and A.id = R.end) as t1
 where t1.level >= 2 and t1.level <= 3
 order by t1.level, t1.name;
 
+
+
 with recursive E_shortest
      as (select
                 *
@@ -78,6 +77,110 @@ with recursive E_shortest
          from   (E_shortest S join e_movie_actor E on E.start_actor = S.end_actor) as prev)
 
 select * from E_shortest;
+
+select A.name, 1 as level from (
+select ma1.end_actor
+from E_movie_actor as ma1
+where ma1.start_actor = 1598) as t1 join actor A on (A.id = t1.end_actor)
+where t1.end_actor != 1598
+order by A.name;
+
+select A.name, 2 as level from (
+select ma2.end_actor
+from E_movie_actor as ma1 join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+where ma1.start_actor = 1598
+
+except
+
+select ma1.end_actor
+from E_movie_actor as ma1
+
+where ma1.start_actor = 1598) as t2 join actor A on (A.id = t2.end_actor)
+where t2.end_actor  != 1598
+order by A.name;
+
+
+select A.name, 3 as level from (
+select ma3.end_actor
+from E_movie_actor as ma1 join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                          join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+where ma1.start_actor = 1598
+
+except
+
+select ma2.end_actor
+from E_movie_actor as ma1 join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+where ma1.start_actor = 1598) as t3 join actor A on (A.id = t3.end_actor)
+where t3.end_actor  != 1598
+order by A.name;
+
+
+select A.name, 4 as level from (
+select ma4.end_actor
+from E_movie_actor as ma1 join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                          join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+                            join E_movie_actor as ma4 on (ma3.end_actor = ma4.start_actor)
+where ma1.start_actor = 1598
+
+except
+
+select ma3.end_actor
+from E_movie_actor as ma1 join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                          join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+where ma1.start_actor = 1598) as t4 join actor A on (A.id = t4.end_actor)
+where t4.end_actor != 1598
+order by A.name;
+
+
+
+select distinct A.name, 5 as level
+from (
+      select distinct ma5.end_actor from E_movie_actor as ma1
+                            join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                            join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+                            join E_movie_actor as ma4 on (ma3.end_actor = ma4.start_actor)
+                            join E_movie_actor as ma5 on (ma4.end_actor = ma5.start_actor)
+          where ma1.start_actor = 369
+            except
+        select distinct ma4.end_actor from E_movie_actor as ma1
+                            join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                            join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+                            join E_movie_actor as ma4 on (ma3.end_actor = ma4.start_actor)
+            where ma1.start_actor = 369
+
+
+        ) as res
+ join actor A on (A.id = res.end_actor)
+where res.end_actor != 369
+order by A.name;
+
+
+select distinct A.name, 6 as level
+from (
+         select t1.end_actor from ((select distinct ma5.end_actor as end from E_movie_actor as ma1
+                            join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                            join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+                            join E_movie_actor as ma4 on (ma3.end_actor = ma4.start_actor)
+                            join E_movie_actor ma5 on (ma4.end_actor = ma5.start_actor)
+          where ma1.start_actor = 369) as checkpoint
+        join F_actor_actor as ma6 on (checkpoint.end= ma6.start_actor)) as t1
+            except
+        (select distinct ma5.end_actor as end from E_movie_actor as ma1
+                            join E_movie_actor as ma2 on (ma1.end_actor = ma2.start_actor)
+                            join E_movie_actor as ma3 on (ma2.end_actor = ma3.start_actor)
+                            join E_movie_actor as ma4 on (ma3.end_actor = ma4.start_actor)
+                            join E_movie_actor as ma5 on (ma4.end_actor = ma5.start_actor)
+            where ma1.start_actor = 369)
+
+
+        ) as res
+ join actor A on (A.id = res.end_actor)
+where res.end_actor != 369
+order by A.name;
+
+create or replace view F_actor_actor (start_actor, end_actor) as
+select a1.actor_id as start_actor, a2.actor_id as end_actor
+from acting a1 join acting a2 on (a1.movie_id = a2.movie_id and a1.actor_id != a2.actor_id);
 
 
 
